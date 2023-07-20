c2395edc253b3d0582763cf83fce7e064202c4e4
Sergey <santerakis@gmail.com>
2023-07-20 19:29:39 +0300

    button

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
+
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
