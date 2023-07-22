8f5f1b1502390e9bc31e7675ccf330e5c6a5eb26
Sergey <santerakis@gmail.com>
2023-07-22 22:37:47 +0300

    next step

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -23,13 +23,32 @@
 //   )
 // }
 
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+// import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+//
+// import s from './button.module.scss'
+//
+// export type ButtonProps<T extends ElementType = 'button'> = {
+//   as?: T
+//   children: ReactNode
+//   variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+//   fullWidth?: boolean
+//   className?: string
+// } & ComponentPropsWithoutRef<T>
+//
+// export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+//   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+//
+//   return (
+//     <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+//   )
+// }
+
+import { ComponentPropsWithoutRef, ElementType } from 'react'
 
 import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
-  children: ReactNode
   variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
   fullWidth?: boolean
   className?: string
