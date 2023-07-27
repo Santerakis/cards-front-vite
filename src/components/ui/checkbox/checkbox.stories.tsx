d7f7c15f3d05e597352708db956d307da0c7f48e
Sergey <santerakis@gmail.com>
2023-07-27 18:58:05 +0300

    checkbox_completed

---
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta } from '@storybook/react'
+
+import { Checkbox } from './checkbox'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+}
+//} satisfies Meta<typeof Checkbox>
+
+export const Uncontrolled = {
+  args: {
+    label: 'Click here',
+    disabled: false,
+  },
+}
