ec05b6432f62640a3d29a6d8a58afcd5bcc900df
Sergey <santerakis@gmail.com>
2023-08-23 21:55:12 +0300

    privateRoutes

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, RouteObject } from 'react-router-dom'
 
-const router = createBrowserRouter([
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
   {
     path: '/',
     element: <div>hello</div>,
   },
-])
+]
+
+const router = createBrowserRouter([...privateRoutes, ...publicRoutes])
 
 export const Router = () => {
   return <RouterProvider router={router} />
