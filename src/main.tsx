a24335c85fa2123a454cdffbdfaa47845895990d
Sergey <santerakis@gmail.com>
2023-08-24 10:39:14 +0300

    base-api

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,9 @@ import { App } from './App.tsx'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 )
