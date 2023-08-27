2dd345c7ef3735a03b6dc5a505112af5a07af137
Sergey <santerakis@gmail.com>
2023-08-27 22:15:26 +0300

    store decks settings

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import { baseApi } from '@/services/base-api.ts'
+import { decksReducer } from '@/services/decks/decks-slice.ts'
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    decks: decksReducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
 })
