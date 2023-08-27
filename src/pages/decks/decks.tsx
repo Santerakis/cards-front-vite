8c410d40eb9128c5e1fadfb4bd8c9db57ea227ab
Sergey <santerakis@gmail.com>
2023-08-27 22:48:08 +0300

    decks simple pagination

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from '@/components/ui'
 import {
   Table,
   TableBody,
@@ -7,16 +8,40 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { useGetDecksQuery } from '@/services/decks'
+import { decksActions } from '@/services/decks/decks-slice.ts'
+import { useAppDispatch, useAppSelector } from '@/services/store.ts'
 
 export const Decks = () => {
+  const dispatch = useAppDispatch()
+
+  const itemsPerPage = useAppSelector(state => state.decks.itemsPerPage)
+  const currentPage = useAppSelector(state => state.decks.currentPage)
+  const searchByName = useAppSelector(state => state.decks.searchByName)
+
+  const setItemsPerPage = (args: number) => dispatch(decksActions.setItemsPerPage(args))
+  const setCurrentPage = (args: number) => dispatch(decksActions.setCurrenPage(args))
+  const setSearchByName = (args: string) => dispatch(decksActions.setSearchByName(args))
+
   const { data, isLoading } = useGetDecksQuery({
-    itemsPerPage: 15,
-    // authorId: 'fe158fab-0656-43b4-953b-7a851330b10d',
+    itemsPerPage,
+    currentPage,
+    name: searchByName,
   })
 
   return (
     <div>
       isLoading: {isLoading.toString()}
+      <div>
+        <Button onClick={() => setItemsPerPage(10)}>itemsPerPge: 10</Button>
+        <Button onClick={() => setItemsPerPage(20)}>itemsPerPge: 20</Button>
+        <Button onClick={() => setItemsPerPage(30)}>itemsPerPge: 30</Button>
+      </div>
+      <div>
+        <Button onClick={() => setCurrentPage(1)}>currentPage: I</Button>
+        <Button onClick={() => setCurrentPage(2)}>currentPage: II</Button>
+        <Button onClick={() => setCurrentPage(3)}>currentPage: III</Button>
+      </div>
+      <TextField value={searchByName} onChange={e => setSearchByName(e.currentTarget.value)} />
       <Table>
         <TableHead>
           <TableRow>
